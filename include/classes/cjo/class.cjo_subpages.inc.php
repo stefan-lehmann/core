16e3221f7960abef539aa133006194a467230085
sl <sl@coeno.com>
2012-06-13 23:53:42 +0200

    fixed subpages class

--- include/classes/cjo/class.cjo_subpages.inc.php
+++ include/classes/cjo/class.cjo_subpages.inc.php
@@ -290,12 +290,30 @@ class cjoSubPages {
     	}
 
     	if ($addon_index) {
+    	    
     		if ($this->debug) echo '1.) '.$CJO['ADDON_PATH'].'/'.$subpage.'/pages/index.inc.php<br/>';
-    		return $CJO['ADDON_PATH'].'/'.$subpage.'/pages/index.inc.php';
+    		
+    		if (file_exists($CJO['ADDON_PATH'].'/'.$subpage.'/pages/index.inc.php'))
+    		    return $CJO['ADDON_PATH'].'/'.$subpage.'/pages/index.inc.php';
+    		  
+            if (file_exists($CJO['ADDON_CONFIG_PATH'].'/'.$subpage.'/pages/index.inc.php'))
+                return $CJO['ADDON_CONFIG_PATH'].'/'.$subpage.'/pages/index.inc.php';                   
+            
+            return $CJO['INCLUDE_PATH'].'/pages/edit/structure.inc.php'; 
+              		  
     	}
     	elseif (!empty($CJO['ADDON']['status'][$this->mypage]) && $subpage != '') {
+    	    
     		if ($this->debug) echo '2.) '.$CJO['ADDON_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php<br/>';
-    		return $CJO['ADDON_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php';
+    		
+            if (file_exists($CJO['ADDON_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php'))
+                return $CJO['ADDON_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php';      		
+
+            if (file_exists($CJO['ADDON_CONFIG_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php'))
+                return $CJO['ADDON_CONFIG_PATH'].'/'.$this->mypage.'/pages/'.$subpage.'.inc.php';  
+                
+            return $CJO['INCLUDE_PATH'].'/pages/edit/structure.inc.php';      
+              
     	}
     	else if ($subpage != '' || $subpage === 0) {
     		if ($this->debug) echo '3.) '.$CJO['INCLUDE_PATH'].'/pages/'.$this->mypage.'/'.$subpage.'.inc.php<br/>';
