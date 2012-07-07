99e77d48171e2cfde9815269513274aecd73a79f
sl <sl@coeno.com>
2012-07-07 22:48:54 +0200

    boilerplate added

--- addons/modul_installer/setup/templates/3.template.php
+++ addons/modul_installer/setup/templates/3.template.php
@@ -1,23 +1,26 @@
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!--[if lt IE 7 ]> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" class="ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" class="ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" class="ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" class="ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de"> <!--<![endif]-->
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-<title>CJO_META[title] | CJO_SERVERNAME</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta name="keywords" content="CJO_META[keywords]" />
-<meta name="description" content="CJO_META[description]" />
-<meta name="audience" content="All" />
-<meta name="copyright" content="" />
-<meta name="Robots" content="index,follow" />
-<meta name="Language" content="Deutsch" />
-<meta name="revisit-after" content="5 Days" />
-<meta name="Content-Language" content="de" />
-<link type="text/css" href="./page/css/standard.css" rel="stylesheet" />
-<script type="text/javascript" src="./page/js/standard.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>CJO_META[title] | CJO_SERVERNAME</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="CJO_META[keywords]">
+    <meta name="description" content="CJO_META[description]">
+    <meta name="audience" content="All">
+    <meta name="copyright" content="">
+    <meta name="Robots" content="index,follow">
+    <meta name="Language" content="Deutsch">
+    <meta name="revisit-after" content="5 Days">
+    <meta name="Content-Language" content="CJO_CLANG_ISO">
+    <meta name="viewport" content="width=device-width">
+    <link type="text/css" href="./page/css/standard.css" rel="stylesheet">
+    <script type="text/javascript" src="./page/js/standard.js"></script>
 </head>
+
 <body id="layout_CJO_TEMPLATE_ID" class="article_CJO_ARTICLE_ID">
 <a name="oben" class="hidden_anchor"></a>
     <div id="page_margin">
