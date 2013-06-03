f8e077abc9a3ba536e581810396b82a2725c6b41
sl <sl@coeno.com>
2013-06-03 10:19:11 +0200

    cjo_get('article_id',...) changed in community addon config

--- addons/community/config.inc.php
+++ addons/community/config.inc.php
@@ -88,7 +88,7 @@ require_once $CJO['ADDON_PATH'].'/'.$mypage.'/classes/class.cm_imexport.inc.php'
 require_once $CJO['ADDON_PATH'].'/'.$mypage.'/classes/class.cm_template.inc.php';
 include_once $CJO['ADDON_PATH'].'/'.$mypage.'/classes/class.cm_bounce.inc.php'; 
 
-if ($CJO['ADDON']['settings'][$mypage]['LOGOUT'] == $article_id) {
+if ($CJO['ADDON']['settings'][$mypage]['LOGOUT'] == cjo_get('article_id', 'cjo-article-id')) {
 	$CJO['LOGOUT'] = true;
 }
 
